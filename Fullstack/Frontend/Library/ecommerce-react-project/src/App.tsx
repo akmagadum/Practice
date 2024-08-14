90536f90f20799d5b213280c6cd27a51b7c035ce
akmagadum <akshaykmagadum9@gmail.com>
2024-08-14 23:35:37 +0530

    k

--- Fullstack/Frontend/Library/ecommerce-react-project/src/App.tsx
+++ Fullstack/Frontend/Library/ecommerce-react-project/src/App.tsx
@@ -1,4 +1,16 @@
+import { Button } from "./components/Button"
+import { LoopinReact } from "./components/LoopinReact"
+import { Home } from "./pages/Home"
+import { Login } from "./pages/Login"
+import Allroutes from "./routes"
+
+
 let App=()=>{
-  return <h1>Akshay Magadum</h1>
+
+  return <div>
+    <Allroutes/>
+  </div>
+  
 }
 export default App
+
